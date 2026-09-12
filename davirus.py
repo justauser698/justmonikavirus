7484ebc6608417600555836a8412ce9f76178ed2
Justauser698 <Dawidmika68@gmail.com>
2026-09-12 03:26:26 +0200

    made so my friends will not hate me lol

--- davirus.py
+++ davirus.py
@@ -26,6 +26,7 @@ if platform.system() == "Windows":
 else:
     terminals = [
         ["gnome-terminal", "--", sys.executable, os.path.abspath(__file__)],
+        # If ur my friend and hate gnome well i also dont wanna put this here but i need to ok
         ["konsole", "-e", sys.executable, os.path.abspath(__file__)],
         ["kitty", "-e", sys.executable, os.path.abspath(__file__)],
         ["xfce4-terminal", "-e", f"{sys.executable} {os.path.abspath(__file__)}"],
--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if platform.system() == "Windows":
 else:
     terminals = [
         ["gnome-terminal", "--", sys.executable, script_path],
+        # If ur my friend and hate gnome well i also dont wanna put this here but i need to ok
         ["konsole", "-e", sys.executable, script_path],
         ["kitty", "-e", sys.executable, script_path],
         ["xfce4-terminal", "-e", f"{sys.executable} {script_path}"],
